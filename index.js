51218b4799a1a3dd5a8da2fb0bcc8d7e28bb65da
JubtaroKujub <41028399+JubtaroKujub@users.noreply.github.com>
2020-02-26 20:54:25 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var Twit = require('twit')
+var T = new Twit({
+  consumer_key:         'JqOtNk4LCzm9GbzSQ8bbB8EeJ',
+  consumer_secret:      'z6r92ByboUpEtIWZXJG3TxKiorSi4exJSfqfutBm8JkSxtdGKy',
+  access_token:         '1226559082812723200-DnI3kxqEYgZ7zTR9tvRe28b8wWB4nW',
+  access_token_secret:  'grfUgJsM3lyBC2H9fPqwMSv5au2tIRrwy7K3A9lutaYpm',
+})
+var stream = T.stream('statuses/filter', {track: ['#SG_FIL']})
+
+stream.on('tweet', function(tweet){
+  console.log(tweet);
+  T.post('collections/entries/add',{id: '1229985985926033408', tweet_id: tweet.id_str}, function (err, data, response) {
+            console.log(data)
+  })
+})
