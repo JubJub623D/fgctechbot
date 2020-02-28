eb1eae072d2e381f68ad0bd2e50879d3de892b86
JubtaroKujub <41028399+JubtaroKujub@users.noreply.github.com>
2020-02-27 17:23:02 -0800

    Add files via upload
    fix to id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var stream = T.stream('statuses/filter', {track: ['#SG_FIL']})
 
 stream.on('tweet', function(tweet){
   console.log(tweet);
-  T.post('collections/entries/add',{id: '1229985985926033408', tweet_id: tweet.id_str}, function (err, data, response) {
+  T.post('collections/entries/add',{id: 1229985985926033408, tweet_id: tweet.id_str}, function (err, data, response) {
             console.log(data)
   })
 })
